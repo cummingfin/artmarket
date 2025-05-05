dcd8e3d65434f649f32e6f65f94719d6eddd4214
Fin Cumming <fincumming@Fins-MacBook-Air.local>
2025-05-05 23:26:46 +0100

    white background

--- pages/messages/[id].tsx
+++ pages/messages/[id].tsx
@@ -91,7 +91,7 @@ if (messages.length > 0) {
   return (
     <>
       <Navbar />
-      <div className="max-w-2xl mx-auto p-4 text-black">
+      <div className="max-w-2xl mx-auto p-4 text-black bg-white min-h-screen">
         <h1 className="text-xl font-bold mb-4">Messages</h1>
 
         <div className="space-y-3 mb-4">
